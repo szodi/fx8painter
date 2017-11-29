bfb7fd4bfdbeab23cfdbbac58ebd26f44d37ddb2
Sándor Szodoray <sandor.szodoray@gmail.com>
2017-11-29 07:11:59 +0100

    CubicCurveManipulator refactored

--- painter2/src/examples/CubicCurveManipulator.java
+++ painter2/src/examples/CubicCurveManipulator.java
@@ -2,7 +2,6 @@ package examples;
 
 import javafx.application.Application;
 import javafx.beans.property.DoubleProperty;
-import javafx.event.EventHandler;
 import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
@@ -77,6 +76,9 @@ public class CubicCurveManipulator extends Application
 	// a draggable anchor displayed around a point.
 	class Anchor extends Circle
 	{
+		double dragDeltaX = 0.0;
+		double dragDeltaY = 0.0;
+
 		Anchor(Color color, DoubleProperty x, DoubleProperty y)
 		{
 			super(x.get(), y.get(), 10);
@@ -87,77 +89,54 @@ public class CubicCurveManipulator extends Application
 
 			x.bind(centerXProperty());
 			y.bind(centerYProperty());
-			enableDrag();
+
+			setOnMousePressed(this::handleMousePressed);
+			setOnMouseReleased(this::handleMouseReleased);
+			setOnMouseDragged(this::handleMouseDragged);
+			setOnMouseEntered(this::handleMouseEntered);
+			setOnMouseExited(this::handleMouseExited);
 		}
 
-		// make a node movable by dragging it around with the mouse.
-		private void enableDrag()
+		private void handleMousePressed(MouseEvent mouseEvent)
 		{
-			final Delta dragDelta = new Delta();
-			setOnMousePressed(new EventHandler<MouseEvent>()
-			{
-				@Override
-				public void handle(MouseEvent mouseEvent)
-				{
-					// record a delta distance for the drag and drop operation.
-					dragDelta.x = getCenterX() - mouseEvent.getX();
-					dragDelta.y = getCenterY() - mouseEvent.getY();
-					getScene().setCursor(Cursor.MOVE);
-				}
-			});
-			setOnMouseReleased(new EventHandler<MouseEvent>()
-			{
-				@Override
-				public void handle(MouseEvent mouseEvent)
-				{
-					getScene().setCursor(Cursor.HAND);
-				}
-			});
-			setOnMouseDragged(new EventHandler<MouseEvent>()
-			{
-				@Override
-				public void handle(MouseEvent mouseEvent)
-				{
-					double newX = mouseEvent.getX() + dragDelta.x;
-					if (newX > 0 && newX < getScene().getWidth())
-					{
-						setCenterX(newX);
-					}
-					double newY = mouseEvent.getY() + dragDelta.y;
-					if (newY > 0 && newY < getScene().getHeight())
-					{
-						setCenterY(newY);
-					}
-				}
-			});
-			setOnMouseEntered(new EventHandler<MouseEvent>()
-			{
-				@Override
-				public void handle(MouseEvent mouseEvent)
-				{
-					if (!mouseEvent.isPrimaryButtonDown())
-					{
-						getScene().setCursor(Cursor.HAND);
-					}
-				}
-			});
-			setOnMouseExited(new EventHandler<MouseEvent>()
-			{
-				@Override
-				public void handle(MouseEvent mouseEvent)
-				{
-					if (!mouseEvent.isPrimaryButtonDown())
-					{
-						getScene().setCursor(Cursor.DEFAULT);
-					}
-				}
-			});
+			dragDeltaX = getCenterX() - mouseEvent.getX();
+			dragDeltaY = getCenterY() - mouseEvent.getY();
+			getScene().setCursor(Cursor.MOVE);
 		}
 
-		// records relative x and y co-ordinates.
-		private class Delta
+		private void handleMouseReleased(MouseEvent mouseEvent)
 		{
-			double x, y;
+			getScene().setCursor(Cursor.HAND);
+		}
+
+		private void handleMouseDragged(MouseEvent mouseEvent)
+		{
+			double newX = mouseEvent.getX() + dragDeltaX;
+			if (newX > 0 && newX < getScene().getWidth())
+			{
+				setCenterX(newX);
+			}
+			double newY = mouseEvent.getY() + dragDeltaY;
+			if (newY > 0 && newY < getScene().getHeight())
+			{
+				setCenterY(newY);
+			}
+		}
+
+		private void handleMouseEntered(MouseEvent mouseEvent)
+		{
+			if (!mouseEvent.isPrimaryButtonDown())
+			{
+				getScene().setCursor(Cursor.HAND);
+			}
+		}
+
+		private void handleMouseExited(MouseEvent mouseEvent)
+		{
+			if (!mouseEvent.isPrimaryButtonDown())
+			{
+				getScene().setCursor(Cursor.DEFAULT);
+			}
 		}
 	}
 }
